63f121d5f1ec6b721fd96a9906d2d66e6147f413
tweis34 <tylerweis34@gmail.com>
2020-05-27 13:00:28 -0500

    fixed the celestial objs

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,9 +5,9 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects;
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            public DbSet<CelestialObject> CelestialObjects;
         }
     }
 }
