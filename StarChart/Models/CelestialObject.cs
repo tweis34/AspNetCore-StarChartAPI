b543b6fc025909c44903834da36fd453e12c5479
tweis34 <tylerweis34@gmail.com>
2020-05-27 13:03:56 -0500

    turned them into properties

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -5,12 +5,12 @@ using System;
 
 namespace StarChart.Models{
     public class CelestialObject{
-        public int Id;
+        public int Id {get;set;}
         [Required]
-        public string Name;
-        public int? OrbitedObjectId;
+        public string Name {get;set;}
+        public int? OrbitedObjectId {get;set;}
         [NotMapped]
-        public List<CelestialObject> Satellites;
-        public TimeSpan OrbitalPeriod;
+        public List<CelestialObject> Satellites {get;set;}
+        public TimeSpan OrbitalPeriod {get;set;}
     }
 }
